f05a07ed82b25a8fa654d048c0b3717b026d0ad4
Andres Munoz <muza.andres@gmail.com>
2026-01-20 13:54:43 -0600

    Add code from replit

---
+++ script.js
@@ -0,0 +1,10 @@
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
