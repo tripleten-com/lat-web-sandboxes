a1bd541483ae6555944f190409a996b42b64a9e0
Vladislav Pavlov <vldslw@gmail.com>
2026-04-16 12:02:26 -0300

    add comments

--- script.ts
+++ script.ts
@@ -9,20 +9,26 @@ class Card {
   }
 
   private getTemplate(): HTMLElement {
+    // Buscamos el template y le aseguramos a TS que es un HTMLTemplateElement
     const cardTemplate = document.querySelector(
       "#card-template",
     ) as HTMLTemplateElement;
 
+    // Clonamos el contenido. El "!" tras querySelector asegura que el elemento existe en el HTML.
+    // Usamos "as HTMLElement" porque cloneNode devuelve un tipo Node genérico.
     const cardElement = cardTemplate.content
       .querySelector(".card")!
       .cloneNode(true) as HTMLElement;
 
+    // Devolvemos el elemento clonado, listo para ser modificado con los datos específicos de esta instancia.
     return cardElement;
   }
 
   generateCard(): HTMLElement {
+    // Obtenemos el marcado vacío
     this.element = this.getTemplate();
 
+    // Buscamos los elementos internos y les asignamos su tipo específico.
     const cardAvatar = this.element.querySelector(
       ".card__avatar",
     ) as HTMLImageElement;
@@ -30,17 +36,21 @@ class Card {
       ".card__paragraph",
     ) as HTMLElement;
 
+    // Inyectamos los datos únicos de esta instancia
     cardAvatar.src = this.image;
     cardParagraph.textContent = this.text;
 
+    // Devolvemos el elemento finalizado listo para ser insertado en el DOM.
     return this.element;
   }
 }
 
+// Creamos la instancia (el objeto) con sus datos específicos
 const card = new Card(
   "¡Hola! ¿Cómo estás?",
   "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_card__image.jpg",
 );
 
+// Generamos el elemento HTML y lo añadimos a la página
 const cardElement = card.generateCard();
 document.body.append(cardElement);
