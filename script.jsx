1b9409f779af1dfe24cc13f45d229ad751ca452c
Vladislav Pavlov <vldslw@gmail.com>
2026-06-10 10:35:03 -0300

    add lesson 7 code

--- script.jsx
+++ script.jsx
@@ -1,7 +1,14 @@
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-  <div>
-    <h2>JSX: Sintaxis básica</h2>
-  </div>
+  <>
+    <div>
+      <h2>Alejandro</h2>
+      <p>Desarrollador Web</p>
+    </div>
+    <div>
+      <h2>Laura</h2>
+      <p>Cientifica de Datos</p>
+    </div>
+  </>
 );
